943a808710bb994949f22e55e4431bac9b650f94
Krzysztof Urban <krzysztof.urban@netguru.pl>
2014-01-28 17:39:54 +0100

    get rid of deprecation warnings

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Review
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.i18n.enforce_available_locales = true
+
     config.autoload_paths += %W(#{config.root}/app/parsers)
   end
 end
