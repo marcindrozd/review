21cb58694c1a1ce1697e92b143d41e636f712988
Marcin Stecki <marcin@netguru.pl>
2014-01-28 16:26:29 +0100

    typo

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,7 +1,7 @@
 require 'rollbar/rails'
 
 Rollbar.configure do |config|
-  config.access_token = ENV['SECRET_KEY_BASE']
+  config.access_token = ENV['ROLLBAR_TOKEN']
 
   if Rails.env.test? || Rails.env.development?
     config.enabled = false
