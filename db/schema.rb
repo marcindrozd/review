99dc4bf465ec18f807c95203cf446deb07740658
blazejhadzik <blazej.hadzik@netguru.pl>
2014-02-26 18:34:17 +0100

    schema [#64154516]

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140129144731) do
+ActiveRecord::Schema.define(version: 20140226142537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "commit_users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "commits", force: true do |t|
     t.string   "remote_id"
@@ -24,8 +32,10 @@ ActiveRecord::Schema.define(version: 20140129144731) do
     t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "passed_at"
+    t.integer  "commit_user_id"
   end
 
+  add_index "commits", ["commit_user_id"], name: "index_commits_on_commit_user_id", using: :btree
   add_index "commits", ["project_id"], name: "index_commits_on_project_id", using: :btree
 
   create_table "commits_tickets", id: false, force: true do |t|
@@ -47,7 +57,11 @@ ActiveRecord::Schema.define(version: 20140129144731) do
 
   create_table "projects", force: true do |t|
     t.string "name"
+    t.string "token", default: "", null: false
+    t.string "url",   default: "", null: false
   end
+
+  add_index "projects", ["token"], name: "index_projects_on_token", using: :btree
 
   create_table "tickets", force: true do |t|
     t.string "remote_id"
