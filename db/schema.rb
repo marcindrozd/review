9726e4ecef0060dc646b451f961c843d5ca80b90
blazejhadzik <blazej.hadzik@netguru.pl>
2014-02-27 12:29:17 +0100

    dropped default values [9765debea8e056b99d4c428bb748bcec05dd2139] [#64154516]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140226142537) do
+ActiveRecord::Schema.define(version: 20140227111729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
