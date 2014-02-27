496abc9f13aa7108fd1f2e466655ab928f44bd3a
blazejhadzik <blazej.hadzik@netguru.pl>
2014-02-27 18:53:35 +0100

    fixes [#64154516]

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,3 @@
 class Person < ActiveRecord::Base
-  has_many :authored_commits, class_name: 'Commit', inverse_of: :author
+  has_many :authored_commits, class_name: 'Commit', inverse_of: :author, foreign_key: 'author_id'
 end
