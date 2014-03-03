61024c21b1434446bb4676d3d26118663dc0e24e
Krzysztof Urban <krzysztof.urban@netguru.pl>
2014-03-03 19:22:21 +0100

    add avatar url to person serializer

--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -1,4 +1,8 @@
 class PersonSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id, :name
+  attributes :id, :name, :image_url
+
+  def image_url
+    Gravatar.new(object.email).image_url
+  end
 end
