1d4b49b70a201ed4bfcff95d96d6cdd4b4283308
Krzysztof Urban <krzysztof.urban@netguru.pl>
2014-03-04 18:45:53 +0100

    Start moving adding new commits from parser from Commit model to service object

---
+++ app/services/commits_from_commit_parsers.rb
@@ -0,0 +1,27 @@
+class CommitsFromCommitParsers
+
+  attr_reader :commit_parsers
+
+  def initialize(commit_parsers)
+    @commit_parsers = commit_parsers
+  end
+
+  def create
+    add_batch_remote
+  end
+
+  private
+
+  def add_batch_remote
+    commit_parsers.map{ |commit_parser| add_remote commit_parser }.compact
+  end
+
+  def add_remote(commit_parser)
+    unless Commit.where(remote_id: commit_parser.remote_id).exists?
+      Commit.create(commit_parser.attributes) do |commit|
+        commit.add_remote_tickets commit_parser.tickets
+        commit.set_commit_author commit_parser.author
+      end
+    end
+  end
+end
