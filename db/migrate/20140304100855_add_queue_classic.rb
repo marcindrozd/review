75f786e23b5a529c6dadce52c1a43c4042642ab4
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-04 14:52:09 +0100

    fixing migrations [#64154500]

--- db/migrate/20140304100855_add_queue_classic.rb
+++ db/migrate/20140304100855_add_queue_classic.rb
@@ -1,9 +1,12 @@
 class AddQueueClassic < ActiveRecord::Migration
-  def self.up
-    QC::Setup.create
-  end
+  def change
+    create_table "queue_classic_jobs", force: true do |t|
+      t.text     "q_name",    null: false
+      t.text     "method",    null: false
+      t.json     "args",      null: false
+      t.datetime "locked_at"
+    end
 
-  def self.down
-    QC::Setup.drop
+    add_index "queue_classic_jobs", [:q_name, :id]
   end
 end
--- db/migrate/20140304130754_drop_queue_classic.rb
+++ db/migrate/20140304130754_drop_queue_classic.rb
@@ -1,5 +1,15 @@
 class DropQueueClassic < ActiveRecord::Migration
   def change
-    QC::Setup.drop
+    reversible do |dir|
+      dir.down do
+        add_index "queue_classic_jobs", [:q_name, :id]
+      end
+    end
+    drop_table "queue_classic_jobs", force: true do |t|
+      t.text     "q_name",    null: false
+      t.text     "method",    null: false
+      t.json     "args",      null: false
+      t.datetime "locked_at"
+    end
   end
 end
