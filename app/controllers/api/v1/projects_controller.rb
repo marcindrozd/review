591e0e22e22ce5fb8c7946ae8cb969cd885a75d1
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-05 15:33:00 +0100

    path change [#64154516]

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::ProjectsController < Api::V1::BaseController
   before_filter :auth_token
-  expose(:project)
+  expose(:project) { Project.from_token(token).first }
 
-  def show
+  def index
     render json: project, serializer: Api::V1::ProjectSerializer
   end
 
   private
   def auth_token
-    render json: { message: t('.no_access') }, status: 401 unless token && Project.from_token(token).exists?
+    render json: { message: t('.no_access') }, status: 401 unless token && project.present?
   end
 
   def token
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Review::Application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :projects, only: :show
+      resources :review_check, controller: 'projects', only: [:index]
     end
   end
 
