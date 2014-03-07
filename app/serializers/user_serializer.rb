1f35aabb041f6289be4e883c757fa9b597dc1c5a
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-07 18:09:49 +0100

    model, serializer [#67100244]

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id, :image_url, :nickname, :name, :admin
+  attributes :id, :image_url, :nickname, :name, :admin, :person_id
   has_many :permissions
 end
