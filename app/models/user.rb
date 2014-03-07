60d495768d8f60b64035431a6d5218d08943193e
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-07 18:29:37 +0100

    trying to assign commits' author with user [#67100244]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :permissions
   belongs_to :person
+  before_create :assign_person
+
+  private
+  def assign_person
+    self.person = Person.find_by(email: self.email)
+  end
 end
