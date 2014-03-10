d44ccbcc0b3089c67577f277e3ab7646fdd07e7c
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-10 15:31:38 +0100

    prevent from throwing 500 on test hook [#64154516]

--- app/controllers/payloads/github_hooks_controller.rb
+++ app/controllers/payloads/github_hooks_controller.rb
@@ -1,12 +1,18 @@
 module Payloads
   class GithubHooksController < ActionController::Base
 
+    before_filter :hook_is_valid?, only: :create
     expose(:parser) { GithubHookParser::Main.new params[:payload] }
 
     def create
       ProjectFromRepositoryParser.new(parser.repository).find_or_create
       CommitsFromCommitParsers.new(parser.commits).create
       render :text => 'ok'
+    end
+
+    private
+    def hook_is_valid?
+      render :text => 'wrong arguments' unless parser.is_valid?
     end
   end
 end
