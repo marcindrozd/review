dd46ca6f9c5f495c8ec7f9c98bf7da2e2344a78f
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-12 18:13:59 +0100

    temp fix - sorry [#67306762]

--- app/serializers/commit_fix_serializer.rb
+++
@@ -1,6 +0,0 @@
-class CommitFixSerializer < ActiveModel::Serializer
-  attributes :id, :fixed_commit_id, :fixing_commit_id
-
-  belongs_to :fixed_commit,  class_name: 'Commit', inverse_of: :fixing_commits
-  belongs_to :fixing_commit, class_name: 'Commit', inverse_of: :fixed_commits
-end
