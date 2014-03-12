d8082133200fc7323799c06c25103258c57f7f81
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-12 16:18:46 +0100

    fixing commits associations [#67306762]

--- app/models/fixing_commit.rb
+++ app/models/fixing_commit.rb
@@ -1,4 +1,7 @@
 class FixingCommit < ActiveRecord::Base
   validates :fixing_commit_id, presence: true
   validates :fixed_commit_id,  presence: true
+
+  belongs_to :fixed_commit,  class_name: 'Commit', inverse_of: :fixing_commits
+  belongs_to :fixing_commit, class_name: 'Commit', inverse_of: :fixed_commits
 end
