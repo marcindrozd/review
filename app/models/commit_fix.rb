320f942d75faeaee4d28ddb8afa9a7fff11653b5
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-13 13:25:12 +0100

    improved relations [#67306762]

--- app/models/commit_fix.rb
+++ app/models/commit_fix.rb
@@ -4,4 +4,6 @@ class CommitFix < ActiveRecord::Base
 
   belongs_to :fixed_commit,  class_name: 'Commit', inverse_of: :fixing_commits
   belongs_to :fixing_commit, class_name: 'Commit', inverse_of: :fixed_commits
+  belongs_to :fixes, class_name: "Commit", foreign_key: "fixing_commit_id"
+  belongs_to :fixed, class_name: "Commit", foreign_key: "fixed_commit_id"
 end
