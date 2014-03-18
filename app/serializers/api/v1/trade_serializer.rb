6343e1b13553062bb56ec6af7dcafb8468b9f36e
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-18 16:59:40 +0100

    trade controller, serializer BASICS [#67739206]

---
+++ app/serializers/api/v1/trade_serializer.rb
@@ -0,0 +1,38 @@
+class Api::V1::TradeSerializer < ActiveModel::Serializer
+
+  root false
+
+  def attributes
+    {
+      name: object.name,
+      url: review_url,
+      expiration_date: object.deadline,
+      unreviewed_commits: commits_as_json,
+    }
+  end
+
+  private
+
+  def review_url
+    "http://#{ENV['REVIEW_DOMAIN']}/projects/#{object.id}"
+  end
+
+  def commits_as_json
+    {
+      'by_user'  => commits_by_user,
+      'by_state' => commits_by_state,
+    }
+  end
+
+  def commits_by_state
+    {
+      "pending"         => object.commits.pending.count,
+      "auto_rejected"   => object.commits.auto_rejected.count,
+    }
+  end
+
+  def commits_by_user
+    object.commits.unreviewed.joins(:author).group('people.email').count
+  end
+
+end
