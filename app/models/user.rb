4ea94b458ee3912fbb18cd048721781cc66bf3bd
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-19 13:10:23 +0100

    create token on callback [#67739206]

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,14 @@ class User < ActiveRecord::Base
 
   belongs_to :person
   before_create :assign_person
+  after_create :create_token
 
   private
   def assign_person
     self.person = Person.find_by(email: self.email)
+  end
+
+  def create_token
+    self.tokens.create
   end
 end
