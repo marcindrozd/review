9b39591a55e13019e1c1362e1513b33c78efd51c
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-19 13:01:15 +0100

    api controllers [#67739206]

--- app/controllers/api/v1/trade_controller.rb
+++ app/controllers/api/v1/trade_controller.rb
@@ -1,7 +1,23 @@
 class Api::V1::TradeController < Api::V1::BaseController
-  expose(:projects){ Commit.unreviewed.includes(:project).map(&:project).uniq }
+  before_filter :auth_token
+
+  expose(:user){ Token.get_tokenable(token) }
+  expose(:projects){ Project.for_user user }
+
   def index
     render json: projects, each_serializer: Api::V1::TradeSerializer, root: 'projects'
   end
 
+  private
+  def auth_token
+    deny_access unless token && user.present? && user.instance_of?(User)
+  end
+
+  def deny_access
+    render json: { message: t('api.v1.no_access') }, status: 401
+  end
+
+  def token
+    params[:token]
+  end
 end
