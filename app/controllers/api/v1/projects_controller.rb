bc2b4287129e0043d4fd932ee1727a82ecb87277
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-19 13:01:15 +0100

    api controllers [#67739206]

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ProjectsController < Api::V1::BaseController
   before_filter :auth_token
-  expose(:project) { Project.from_token(token).first }
+  expose(:project) { Token.get_tokenable(token) }
 
   def index
     render json: project, serializer: Api::V1::ProjectSerializer
@@ -8,11 +8,11 @@ class Api::V1::ProjectsController < Api::V1::BaseController
 
   private
   def auth_token
-    deny_access unless token && project.present?
+    deny_access unless token && project.present? && project.instance_of?(Project)
   end
 
   def deny_access
-    render json: { message: t('.no_access') }, status: 401
+    render json: { message: t('api.v1.no_access') }, status: 401
   end
 
   def token
