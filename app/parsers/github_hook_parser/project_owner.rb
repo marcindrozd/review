2ca70167ca71266866ca57fd274b5b035fceb93b
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-20 18:01:41 +0100

    project owner parser [#67936244]

---
+++ app/parsers/github_hook_parser/project_owner.rb
@@ -0,0 +1,23 @@
+class GithubHookParser::ProjectOwner
+
+  def initialize(raw)
+    @raw = raw.with_indifferent_access
+  end
+
+  def attributes
+    {
+      name: name,
+      email: email,
+    }
+  end
+
+  def name
+    @raw.fetch('name')
+  end
+
+  def email
+    @raw.fetch('email')
+  end
+
+end
+
