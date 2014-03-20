30ad8aaf1e8707fd1653e6f8bfae8dadfa2b63fa
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-20 09:49:37 +0100

    fixing [45d7878570f22a17135e8badc9d7b8d000d664c1]

--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -5,22 +5,20 @@ describe Token do
   it { should respond_to(:tokenable) }
   it { should validate_uniqueness_of(:value) }
 
-  describe "model methods" do
+  describe ".get_tokenable" do
     let!(:project){ Project.create(name: 'something', url: 'http://test.com') }
     let!(:token){ project.tokens.first }
 
-    context 'get_tokenable' do
-      it 'returns nil if no token given' do
-        expect(Token.get_tokenable nil).to eq(nil)
-      end
+    it 'returns nil if no token given' do
+      expect(Token.get_tokenable nil).to be_nil
+    end
 
-      it 'returns nil if wrong token given' do
-        expect(Token.get_tokenable 'wrong_token').to eq(nil)
-      end
+    it 'returns nil if wrong token given' do
+      expect(Token.get_tokenable 'wrong_token').to be_nil
+    end
 
-      it 'returns project if correct token given' do
-        expect(Token.get_tokenable token.value).to eq(project)
-      end
+    it 'returns project if correct token given' do
+      expect(Token.get_tokenable token.value).to eq(project)
     end
   end
 end
