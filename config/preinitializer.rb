bf09aefc8605cfc6d453f2b3eefd89bbf9a764af
Radosław Piątek <radoslaw.piatek@netguru.pl>
2015-02-23 23:15:03 +0100

    Application config should be read-only

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -4,4 +4,4 @@ pub_config = ActiveSupport::OrderedOptions.new
 pub_config.deep_merge!(YAML.load(ERB.new(File.read(File.expand_path('../config.yml', __FILE__))).result)[Rails.env])
 sec_config = YAML.load(ERB.new(File.read(File.expand_path('../sec_config.yml', __FILE__))).result)[Rails.env] rescue {}
 
-AppConfig = pub_config.deep_merge(sec_config)
+AppConfig = pub_config.deep_merge(sec_config).freeze
