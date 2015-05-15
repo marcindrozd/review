e5230620c19d58e647ece303b6a9da5c156df017
Jacek Kubiak <jacek.kubiak@netguru.pl>
2015-05-15 11:29:11 +0200

    Properly handle json data from POST request

--- app/controllers/payloads/github_hooks_controller.rb
+++ app/controllers/payloads/github_hooks_controller.rb
@@ -2,7 +2,7 @@ module Payloads
   class GithubHooksController < ActionController::Base
     before_filter :check_if_push_event
 
-    expose(:parser) { GithubHookParser::Main.new params[:payload] }
+    expose(:parser) { GithubHookParser::Main.new request.raw_post }
 
     def create
       ProjectFromRepositoryParser.new(parser.repository).find_or_create
