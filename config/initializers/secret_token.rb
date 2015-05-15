56314c667e585e249f2e44cea9b15aba066cb126
Jacek Kubiak <jacek.kubiak@netguru.pl>
2015-05-15 15:47:47 +0200

    Use AppConfig

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Review::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+Review::Application.config.secret_key_base = AppConfig.secret_key_base
