874e409f2e9bda2f47a0289de8d6be91da2980a1
Jacek Kubiak <jacek.kubiak@netguru.pl>
2015-05-28 09:56:19 +0200

    Update deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,8 @@ set :repo_url,  "git@github.com:netguru/review.git"
 set :deploy_to, ENV['DEPLOY_PATH']
 
 set :linked_files, %w(config/database.yml config/sec_config.yml)
+set :linked_dirs, %w(log tmp vendor/bundle public/uploads review_front/node_modules)
 
-# Make sure that folders listed here are present in /app/shared, copy them if not
-set :linked_dirs, %w(log tmp vendor/bundle public/uploads)
+set :npm_target_path, -> { release_path.join('review_front') }
+set :bower_target_path, -> { release_path.join('review_front') }
+set :ember_cli_target_path, -> { release_path.join('review_front') }
