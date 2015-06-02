9e7d427ba6fb70d550d65d3e5c1d9e212e7210dc
micjez <michal.jezynski@netguru.co>
2015-06-02 14:33:08 +0200

    do not create sym link for bin folder in shared folder[#95966686]

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url,  "git@github.com:netguru/review.git"
 set :deploy_to, ENV['DEPLOY_PATH']
 
 set :linked_files, %w(config/database.yml config/sec_config.yml)
-set :linked_dirs, %w(bin log tmp vendor/bundle public/uploads review_front/bower_components review_front/node_modules)
+set :linked_dirs, %w(log tmp vendor/bundle public/uploads review_front/bower_components review_front/node_modules)
 
 set :npm_target_path, -> { release_path.join('review_front') }
 set :bower_target_path, -> { release_path.join('review_front') }
