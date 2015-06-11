c21307c0569a22235e05588e433a86059a528bb3
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-06-11 18:57:32 +0200

    added pivotal ticket parser test [#96544350]

---
+++ spec/parsers/github_hook_parser/pivotal_ticket_parser_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe GithubHookParser::PivotalTicket do
+  let(:pivotal_ticket) { described_class.new('12345') }
+  let(:message) { "this is a test commit [#54321]" }
+
+  it 'creates a ticket' do
+    expect(pivotal_ticket.url).to eq("https://www.pivotaltracker.com/story/show/12345")
+  end
+
+  it 'extracts ticket id from message' do
+    expect(described_class.from_message(message).first.ticket_id).to eq("54321")
+  end
+end
