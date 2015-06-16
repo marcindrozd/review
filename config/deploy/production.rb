13d7fce9c5a7c5cd0f5218f87d2d0c6717bb9608
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-06-16 13:08:08 +0200

    fixing deploy files for capistrano 3 [7b7b1b7f28ac845765ec4129e095f769ca816f42]

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,3 @@
 server ENV['PRODUCTION_SERVER'], user: ENV['PRODUCTION_USER'], roles: %w(web app db)
-after "deploy:symlink", "deploy:update_crontab"
 
-namespace :deploy do
-  desc "Update the crontab file"
-  task :update_crontab, :roles => :db do
-    run "cd #{release_path} && whenever --update-crontab #{application} --set environment=production"
-  end
-end
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}"  }
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,10 +1,3 @@
 server ENV['STAGING_SERVER'], user: ENV['STAGING_USER'], roles: %w(web app db)
 
-after "deploy:symlink", "deploy:update_crontab"
-
-namespace :deploy do
-  desc "Update the crontab file"
-  task :update_crontab, :roles => :db do
-    run "cd #{release_path} && whenever --update-crontab #{application} --set environment=staging"
-  end
-end
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}"  }
