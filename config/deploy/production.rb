c3d2c8932bdd6ae548b8f28ea226fbb844b7c716
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-06-16 12:54:13 +0200

    updated deploy files [#97001534]

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,9 @@
 server ENV['PRODUCTION_SERVER'], user: ENV['PRODUCTION_USER'], roles: %w(web app db)
+after "deploy:symlink", "deploy:update_crontab"
+
+namespace :deploy do
+  desc "Update the crontab file"
+  task :update_crontab, :roles => :db do
+    run "cd #{release_path} && whenever --update-crontab #{application} --set environment=production"
+  end
+end
