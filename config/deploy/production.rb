58f6b72faa6baebebb07bd6c71333044a84b40a9
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-06-18 17:44:59 +0200

    hiding secrets [c3667f8f6dd7b9b591fbd97350f11e6d41815200]

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
-server ENV['PRODUCTION_SERVER'], user: ENV['PRODUCTION_USER'], roles: %w(web app db)
-
+require 'net/ssh/proxy/command'
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}"  }
+
+server ENV['PRODUCTION_SERVER'], user: ENV['PRODUCTION_USER'], roles: %w(web app db)
+set :ssh_options, proxy: Net::SSH::Proxy::Command.new("ssh '#{ENV['GATEWAY']}' -W %h:%p")
+set :branch, 'production'
