bb536af353dec677f9f153d912fe5da5992274e2
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-06-29 15:59:19 +0200

    updating db.schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629112436) do
+ActiveRecord::Schema.define(version: 20150629135306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "commit_fixes", force: true do |t|
-    t.integer  "fixing_commit_id", null: false
+    t.integer  "fixing_commit_id"
     t.integer  "fixed_commit_id",  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
