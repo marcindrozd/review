d3e8b0639d3d305719c6978ebc0f023481193699
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-06-29 18:50:24 +0200

    moving nginx config information

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, "review"
 set :repo_url,  "git@github.com:netguru/review.git"
 set :deploy_to, ENV['DEPLOY_PATH']
 
-set :linked_files, %w(config/database.yml config/sec_config.yml)
+set :linked_files, %w(config/database.yml config/sec_config.yml config/nginx.production.review.conf)
 set :linked_dirs, %w(log tmp vendor/bundle public/uploads review_front/bower_components review_front/node_modules)
 
 set :npm_target_path, -> { release_path.join('review_front') }
