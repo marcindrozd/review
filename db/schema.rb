3d9743f084c97a90297c278de1d14d8e8ce91b1d
Maciej Radziwon <maciej.radziwon@netguru.co>
2015-07-03 12:01:51 +0200

    Generate migration, update schema
    [#98173854]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630093045) do
+ActiveRecord::Schema.define(version: 20150703095655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,17 @@ ActiveRecord::Schema.define(version: 20150630093045) do
     t.string  "mongo_id"
   end
 
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
+  add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
+
   create_table "tickets", force: true do |t|
     t.string "remote_id"
     t.string "url"
@@ -124,5 +135,12 @@ ActiveRecord::Schema.define(version: 20150630093045) do
     t.integer "person_id"
     t.string  "mongo_id"
   end
+
+  create_table "users_roles", id: false, force: true do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
 
 end
