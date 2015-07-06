0bba9d1c85ae206c2a7efa3683ab09d3da3988df
Maciej Radziwon <maciej.radziwon@netguru.co>
2015-07-06 15:43:01 +0200

    Remove permission model, controller and serializer
    [#98173854]

--- app/models/permission.rb
+++
@@ -1,6 +0,0 @@
-class Permission < ActiveRecord::Base
-  belongs_to :project
-  belongs_to :user
-  validates :user, :project, presence: true
-  validates :user, uniqueness: { scope: :project }
-end
--- app/serializers/permission_serializer.rb
+++
@@ -1,3 +0,0 @@
-class PermissionSerializer < ApplicationSerializer
-  attributes :id, :user_id, :project_id, :allowed
-end
