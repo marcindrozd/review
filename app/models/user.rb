f4255b6baf8d9e3af28547f2ee5e50ca5e90c715
Maciej Radziwon <maciej.radziwon@netguru.co>
2015-07-21 15:13:03 +0200

    Add associations to user model
    [#98173630]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   rolify
   friendly_id :nickname
   has_many :tokens, as: :tokenable
+  has_one :invitation, foreign_key: 'recipient_id'
+  has_many :sent_invitations, class_name: 'Invitation', foreign_key: 'sender_id'
 
   belongs_to :person
   before_create :assign_person
