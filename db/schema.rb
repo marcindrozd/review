5dee038a45c8be6f5dedda49e0fff2ef58e43856
Maciej Radziwon <maciej.radziwon@netguru.co>
2015-07-22 11:42:49 +0200

    Add invitation status migration
    [#98173630]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721091543) do
+ActiveRecord::Schema.define(version: 20150722082214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20150721091543) do
     t.integer  "recipient_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "accepted",     default: false
   end
 
   create_table "people", force: true do |t|
