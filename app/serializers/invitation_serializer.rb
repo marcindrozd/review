388e3c574e11c973f31c2d7e8141b7561ad392d6
Maciej Radziwon <maciej.radziwon@netguru.co>
2015-07-22 11:44:25 +0200

    Add date and status to serializer
    [#98173630]

--- app/serializers/invitation_serializer.rb
+++ app/serializers/invitation_serializer.rb
@@ -1,3 +1,11 @@
 class InvitationSerializer < ApplicationSerializer
-  attributes :id, :email, :role, :project_ids
+  attributes :id, :email, :role, :project_ids, :date, :status
+
+  def status
+    object.accepted ? 'accepted' : 'pending'
+  end
+
+  def date
+    object.created_at.strftime("%d %b. %Y")
+  end
 end
