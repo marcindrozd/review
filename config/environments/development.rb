9120affbdae1617d76d9c2b5fa6b92df44911920
Maciej Radziwon <maciej.radziwon@netguru.co>
2015-07-27 10:23:53 +0200

    Add mailer configs
    [#98173630]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,4 +22,14 @@ Review::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV['gmail_username'],
+    :password => ENV['gmail_password'],
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Review::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   GA.tracker = AppConfig.ga_id
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV['gmail_username'],
+    :password => ENV['gmail_password'],
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
 end
