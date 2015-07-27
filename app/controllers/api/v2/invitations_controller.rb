115d92c9382d1372bcf0238cb03b143d26e90c8b
Maciej Radziwon <maciej.radziwon@netguru.co>
2015-07-27 10:27:19 +0200

    Create invitation and send email
    [#98173630]

--- app/controllers/api/v2/invitations_controller.rb
+++ app/controllers/api/v2/invitations_controller.rb
@@ -1,15 +1,24 @@
 class Api::V2::InvitationsController < Api::V2::BaseController
+  before_action :check_permission
   expose(:invitations)
+  expose(:invitation, attributes: :invitation_params)
   expose(:paginated_invitations) { invitations.order(created_at: :desc).page params[:page] }
 
   def index
     respond_with paginated_invitations, meta: { total_pages: invitation_pages_count }
   end
 
+  def create
+    invitation.save
+    InvitationMailer.invitation_email(invitation, auth_url).deliver
+    respond_with invitation, location: false
+  end
+
   private
 
   def invitation_params
-    params.require(:invitation).permit(:email, :role, :project_ids)
+    params.require(:invitation).permit(:email, :role, project_ids: [])
+      .merge(sender_id: current_user.id)
   end
 
   def invitation_pages_count
