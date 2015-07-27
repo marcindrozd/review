3dd5f811dd037f4c2d709d2cb6b0a0e05c5d3222
Maciej Radziwon <maciej.radziwon@netguru.co>
2015-07-27 10:27:53 +0200

    Add roles from invitation in sessions controller
    [#98173630]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,13 @@ class SessionsController < ApplicationController
   expose(:github_auth_path) { '/auth/github' }
 
   def index
+    check_invitation_token
   end
 
   def create
     user = UserFromAuth.new(auth_hash).find_or_build
     if user.persisted? || user.save
+      invitation_roles(user)
       sign_in user
       redirect_to root_path
     else
@@ -30,6 +32,25 @@ class SessionsController < ApplicationController
   end
 
   private
+
+  def check_invitation_token
+    return unless params[:invitation_token]
+    session[:invitation_token] = params[:invitation_token]
+  end
+
+  def invitation_roles(user)
+    return unless session[:invitation_token]
+    invitation = Invitation.find_by(token: session[:invitation_token])
+    add_invitation_roles(invitation, user)
+    session.delete(:invitation_token)
+  end
+
+  def add_invitation_roles(invitation, user)
+    projects = invitation.project_ids
+    role = invitation.role
+    projects ? projects.each { |id| user.add_role role, Project.find(id) } : user.add_role(role)
+    invitation.update_attributes(accepted: true)
+  end
 
   def auth_hash
     request.env['omniauth.auth']
