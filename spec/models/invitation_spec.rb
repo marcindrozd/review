c4cf97096e1fa55306a96ba01f18828c6ac7e9e7
Maciej Radziwon <maciej.radziwon@netguru.co>
2015-07-28 10:45:46 +0200

    Add new tests to models
    [#98173630]

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -7,6 +7,7 @@ describe Invitation do
   it { should belong_to :recipient }
 
   it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
 
   it 'generates token before user is created' do
     expect(invitation.token).not_to be_empty
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,6 @@ require 'spec_helper'
 
 describe User do
   it { should respond_to(:tokens) }
+  it { should have_one(:invitation) }
+  it { should have_many(:sent_invitations) }
 end
