d70000197cfe6085b9ea4700a791b51223a53364
Maciej Radziwon <maciej.radziwon@netguru.co>
2015-07-28 14:07:47 +0200

    e6d44de7d82ceb705d0eacd6c55acaa58513194e fix
     [#98173630]

--- review_front/app/app.js
+++ review_front/app/app.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
-import LinkView from './overrides/linkto';
 
 var App;
 
--- review_front/app/overrides/linkto.coffee
+++
@@ -1,6 +0,0 @@
-`import Ember from "ember"`
-
-LinkView = Ember.LinkView.reopen
-  attributeBindings: 'data-toggle'
-
-`export default LinkView`
