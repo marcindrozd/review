76a5a0872174e5a1df8b5e7d3c6ef22f9038f8c1
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-07-30 14:51:18 +0200

    fix for review [0211f342a9e95cc9a969ff210fbd72b6d12e1a54]

--- app/controllers/api/v2/projects_controller.rb
+++ app/controllers/api/v2/projects_controller.rb
@@ -1,11 +1,11 @@
 class Api::V2::ProjectsController < Api::V2::BaseController
-  expose(:project_by_name) { Project.find_by(name: params[:name]) }
+  expose(:project_by_name) { Project.where(name: params[:name]) }
   expose(:projects) { find_projects }
   expose(:paginated_projects) { projects.order(name: :asc).page params[:page] }
 
   def index
     if project_by_name.present?
-      respond_with [project_by_name]
+      respond_with project_by_name
     else
       respond_with paginated_projects, meta: { total_pages: project_pages_count }
     end
