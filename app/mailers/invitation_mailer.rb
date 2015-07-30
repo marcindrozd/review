3b62ba3f5ba42154759f37281b889ad134ed4cc9
Maciej Radziwon <maciej.radziwon@netguru.co>
2015-07-30 11:32:16 +0200

    Disable ganalytics in email
    [#100112312]

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,7 +1,9 @@
 class InvitationMailer < ActionMailer::Base
+  include SendGrid
   default from: 'review@app.com'
 
   def invitation_email(invitation, auth_url)
+    sendgrid_disable :ganalytics
     @invitation = invitation
     @auth_url = auth_url + "?invitation_token=#{@invitation.token}"
     mail(to: @invitation.email, subject: 'Invitation to review app')
