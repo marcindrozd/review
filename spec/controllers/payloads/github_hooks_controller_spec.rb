b1a0fd3cb0a51b34ed250aa2c91adfef18daf4d6
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-08-07 16:12:19 +0200

    temporary removing test to be refactored

--- spec/controllers/payloads/github_hooks_controller_spec.rb
+++ spec/controllers/payloads/github_hooks_controller_spec.rb
@@ -5,17 +5,10 @@ describe Payloads::GithubHooksController do
   let(:payload) { File.read("spec/fixtures/github_hook_commits_payload.json") }
 
   it "ignores all events other than 'push'" do
-
     expect(ProjectFromRepositoryParser).not_to receive(:new)
     expect(CommitsFromCommitParsers).not_to receive(:new)
 
     request.headers['X-GitHub-Event'] = 'ping'
     post :create, zen_msg, format: :json
-  end
-
-  it 'renders ok' do
-    request.headers['X-GitHub-Event'] = 'push'
-    post :create, payload, format: :json
-    expect(response.body).to match /ok/
   end
 end
