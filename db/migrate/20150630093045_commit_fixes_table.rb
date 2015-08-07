9272a7122ff8f229e2485b62bca12665c6c077bc
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-08-07 16:17:34 +0200

    removing old migration script leftovers

--- db/migrate/20150630093045_commit_fixes_table.rb
+++
@@ -1,6 +0,0 @@
-class CommitFixesTable < ActiveRecord::Migration
-  def change
-    add_column :commit_fixes, :fixed_mongo_id, :string
-    change_column_null :commit_fixes, :fixed_commit_id, true
-  end
-end
