245d0d818e43081ef19a4c8d857f5a78316690ad
Krzysztof Urban <krzysztof.urban@netguru.pl>
2015-08-20 18:14:51 +0200

    Remove google analytics
    [#101580326]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,6 @@ Review::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  GA.tracker = AppConfig.ga_id
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
