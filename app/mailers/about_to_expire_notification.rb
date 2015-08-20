dc780c323b4649282850d8f45d32e754a9dd7116
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-08-20 14:03:46 +0200

    Sendgrid mailer
    [#100650680]

---
+++ app/mailers/about_to_expire_notification.rb
@@ -0,0 +1,11 @@
+class AboutToExpireNotification < ActionMailer::Base
+  include SendGrid
+  default from: 'review@app.com'
+
+  def soon_to_expire_email(email_data)
+    sendgrid_disable :ganalytics
+    @email = email_data.first
+    @commits = email_data.last
+    mail(to: @email, subject: 'Some of your commits are about to expire')
+  end
+end
