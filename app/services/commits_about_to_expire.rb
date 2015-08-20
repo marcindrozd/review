0f340fbf375466a08738835855301e2f476e662a
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-08-20 14:03:28 +0200

    Service for pulling commits and emails
    [#100650680]

---
+++ app/services/commits_about_to_expire.rb
@@ -0,0 +1,33 @@
+class CommitsAboutToExpire
+
+  attr_reader :emails_with_commits
+
+  def initialize
+    @authors = authors_for_commits_to_expire
+    @commits = commits_soon_to_expire
+  end
+
+  def commits_for_author(author)
+    {
+     author.email => commits_soon_to_expire.where("author_id = ?", author.id)
+    }
+  end
+
+  def commits_for_authors
+    result = {}
+    authors_for_commits_to_expire.each do |author|
+      result.merge!(commits_for_author(author))
+    end
+    return result
+  end
+
+  private
+
+  def authors_for_commits_to_expire
+    commits_soon_to_expire.map { |p| p.author }.uniq
+  end
+
+  def commits_soon_to_expire
+    Commit.soon_to_expire
+  end
+end
