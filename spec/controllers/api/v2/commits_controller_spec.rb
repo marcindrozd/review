33d3a730b9917a269cbe0e76de7d3bb10dd975ad
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-08-31 16:49:06 +0200

    index tests

--- spec/controllers/api/v2/commits_controller_spec.rb
+++ spec/controllers/api/v2/commits_controller_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe Api::V2::CommitsController do
   let(:project) { create(:project, name: 'project1') }
-  let(:commit) { create(:commit, author_id: author.id, state: 'pending') }
+  let!(:commit) { create(:commit, project_id: project.id, author_id: author.id, state: 'pending') }
   let!(:author) { create(:user, email: 'test@test.co') }
   let(:token) { reviewer.tokens.first.value }
-  let(:reviewer) { create(:person) }
+  let(:reviewer) { create(:person, email: 'test2@test.co') }
 
   describe '#update' do
     let(:valid_params) { {state: 'rejected'}  }
@@ -34,4 +34,39 @@ describe Api::V2::CommitsController do
       end
     end
   end
+  describe '#index' do
+    let!(:commit_accepted) { create(:commit, project_id: project.id, author_id: author.id, state: 'accepted') }
+
+    context 'with valid user returns all commits' do
+      let(:params) { { q: 'null', name: project.name} }
+
+      before :each do
+        session[:user_id] = reviewer.id
+        get :index, params: params, :format => :json
+      end
+      it 'responsd with 200 status' do
+        expect(response.status).to eq(200)
+      end
+      it 'returns two commits' do
+        expect(JSON.parse(response.body).fetch('commits').count).to eq(2)
+      end
+    end
+    context 'with valid user teruns all but accepted and fixed' do
+      let!(:commit_accepted) { create(:commit, project_id: project.id, author_id: author.id, state: 'fixed') }
+
+      before :each do
+        session[:user_id] = reviewer.id
+        get :index, q: {state_not_in: ['accepted', 'fixed']}, :format => :json
+      end
+
+      it 'responsd with 200 status' do
+        expect(response.status).to eq(200)
+      end
+      it 'returns one commit' do
+        expect(JSON.parse(response.body).fetch('commits').count).to eq(1)
+      end
+      it 'returns only the pending commit' do
+        expect(JSON.parse(response.body).fetch('commits').first['state']).to eq('pending')
+      end
+    end
 end
