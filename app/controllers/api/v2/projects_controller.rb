851ec325f28bf0a2c78b874f287a51ce16cdbdf3
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-09-03 15:06:25 +0200

    adding checks for contractor

--- app/controllers/api/v2/projects_controller.rb
+++ app/controllers/api/v2/projects_controller.rb
@@ -16,20 +16,33 @@ class Api::V2::ProjectsController < Api::V2::BaseController
   end
 
   def show
-    respond_with project_by_name
+    return unless params[:name]
+      if admin_or_developer?
+        respond_with project_by_name
+      elsif contractor?(project_by_name.first)
+        respond_with project_by_name
+      else
+        respond_with 'forbidden', status: 403, format: :json
+      end
   end
+
 
   private
 
   def find_projects
-    if current_user.has_role?(:admin) || current_user.has_role?(:developer)
+    if admin_or_developer?
       Project.all
     else
       Project.user_resources(current_user)
     end
   end
-
   def project_pages_count
     (projects.count / paginated_projects.default_per_page.to_f).ceil
+  end
+  def admin_or_developer?
+    current_user.has_role?(:admin) || current_user.has_role?(:developer)
+  end
+  def contractor?(project)
+    current_user.has_role? :contractor, project
   end
 end
