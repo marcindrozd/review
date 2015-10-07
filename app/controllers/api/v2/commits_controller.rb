965f015eccf7a8b4b1c7f1dd31c17b31f7b7b176
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-10-07 12:25:39 +0200

    changing commit ordering to authored_at only [#101689698]

--- app/controllers/api/v2/commits_controller.rb
+++ app/controllers/api/v2/commits_controller.rb
@@ -7,7 +7,7 @@ class Api::V2::CommitsController < Api::V2::BaseController
   expose(:ticket)
   expose(:commits) { find_commits.tagged(params[:tag]) }
   expose(:filtered_commits) { commits.ransack(params[:q]).result }
-  expose(:filtered_paginated_commits) { order_commits_if_expires_at_present }
+  expose(:filtered_paginated_commits) { order_commits_by_authored_at }
   expose(:updated_tags) { params[:commit][:tag] || [] }
 
   def index
@@ -69,12 +69,8 @@ class Api::V2::CommitsController < Api::V2::BaseController
     (filtered_commits.count / filtered_paginated_commits.default_per_page.to_f).ceil
   end
 
-  def order_commits_if_expires_at_present
-    if params[:q] == "null"
-      filtered_commits.order(authored_at: :desc).page params[:page]
-    else
-      filtered_commits.order(expires_at: :asc).page params[:page]
-    end
+  def order_commits_by_authored_at
+    filtered_commits.order(authored_at: :desc).page params[:page]
   end
 
   def send_state_notification commit
