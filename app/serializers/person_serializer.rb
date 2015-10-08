fa96a52775c1f6d3ca072be46883f021516b834b
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2015-10-08 12:44:52 +0200

    replacing http with https for gravatar [#101673886]

--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -2,6 +2,6 @@ class PersonSerializer < ApplicationSerializer
   attributes :id, :username, :name, :image_url
 
   def image_url
-    Gravatar.new(object.email).image_url
+    Gravatar.new(object.email).image_url.sub(/http:/, "https:")
   end
 end
