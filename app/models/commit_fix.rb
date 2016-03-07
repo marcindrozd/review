308fbd5618ea4ba02335f728bdbd8f6bf24d6e4c
Andrzej Piątyszek <konto.andrzeja@gmail.com>
2016-03-07 13:55:08 +0100

    ensure project consistency when removing tickets and commit fixes
    [#114942119]

--- app/models/commit_fix.rb
+++ app/models/commit_fix.rb
@@ -1,4 +1,6 @@
 class CommitFix < ActiveRecord::Base
+  before_destroy :ensure_project_consistency
+
   validates :fixing_commit_id, presence: true
   validates :fixed_commit_id,  presence: true
 
@@ -6,4 +8,13 @@ class CommitFix < ActiveRecord::Base
   belongs_to :fixing_commit, class_name: 'Commit', inverse_of: :fixed_commits
   belongs_to :fixes, class_name: "Commit", foreign_key: "fixing_commit_id"
   belongs_to :fixed, class_name: "Commit", foreign_key: "fixed_commit_id"
+
+  private
+
+  def ensure_project_consistency
+    if fixed_commit.project != fixing_commit.project
+      fail ActiveRecord::RecordNotDestroyed,
+        'Commit from one project fixed commit from another project!'
+    end
+  end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,14 @@
 class Ticket < ActiveRecord::Base
+  before_destroy :ensure_project_consistency
+
   has_many :commits_tickets
   has_many :commits, through: :commits_tickets, dependent: :destroy
+
+  private
+
+  def ensure_project_consistency
+    if commits.any? { |c| c.project != commits.first.project }
+      fail ActiveRecord::RecordNotDestroyed, 'Ticket has commits from different projects!'
+    end
+  end
 end
