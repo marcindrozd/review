1d2aa412aca31c97219a3cc12ad2e3a4938dad38
Andrzej Piątyszek <konto.andrzeja@gmail.com>
2016-03-09 08:32:52 +0100

    temporarily send all emails to yourself to test sidekiq
    [115248155]

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -6,6 +6,6 @@ class InvitationMailer < ActionMailer::Base
     sendgrid_disable :ganalytics
     @invitation = invitation
     @auth_url = auth_url + "?invitation_token=#{@invitation.token}"
-    mail(to: @invitation.email, subject: 'Invitation to review app')
+    # mail(to: @invitation.email, subject: 'Invitation to review app')
   end
 end
