1c19f06710a5037e86e9b88198787719f82b0636
Krzysztof Urban <krzysztof.urban@netguru.pl>
2016-03-12 23:38:40 +0100

    Fix broken tests

--- review_front/tests/unit/components/commit-state-label-test.js
+++ review_front/tests/unit/components/commit-state-label-test.js
@@ -11,29 +11,29 @@ test('it renders', function(assert) {
 });
 
 test("'reviewState' has a default value", function(assert) {
-  return ok(this.subject().get('reviewState') === 'unknown', "should have a value of 'unknown'");
+  return assert.ok(this.subject().get('reviewState') === 'unknown', "should have a value of 'unknown'");
 });
 
-test('uses default label style when unrecognized state provided', function() {
-  return ok(this.subject().get('labelClass') === 'label-default', "should have a class of 'label-default'");
+test('uses default label style when unrecognized state provided', function(assert) {
+  return assert.ok(this.subject().get('labelClass') === 'label-default', "should have a class of 'label-default'");
 });
 
-test('has a specific label style for accepted state', function() {
+test('has a specific label style for accepted state', function(assert) {
   this.subject().set('reviewState', 'accepted');
-  return ok(this.subject().get('labelClass') === 'label-success', "should have a class of 'label-success'");
+  return assert.ok(this.subject().get('labelClass') === 'label-success', "should have a class of 'label-success'");
 });
 
-test('has a specific label style for pending state', function() {
+test('has a specific label style for pending state', function(assert) {
   this.subject().set('reviewState', 'pending');
-  return ok(this.subject().get('labelClass') === 'label-info', "should have a class of 'label-info'");
+  return assert.ok(this.subject().get('labelClass') === 'label-info', "should have a class of 'label-info'");
 });
 
-test('has a specific label style for passed state', function() {
+test('has a specific label style for passed state', function(assert) {
   this.subject().set('reviewState', 'passed');
-  return ok(this.subject().get('labelClass') === 'label-warning', "should have a class of 'label-warning'");
+  return assert.ok(this.subject().get('labelClass') === 'label-warning', "should have a class of 'label-warning'");
 });
 
-test('has a specific label style for rejected state', function() {
+test('has a specific label style for rejected state', function(assert) {
   this.subject().set('reviewState', 'rejected');
-  return ok(this.subject().get('labelClass') === 'label-danger', "should have a class of 'label-danger'");
+  return assert.ok(this.subject().get('labelClass') === 'label-danger', "should have a class of 'label-danger'");
 });
