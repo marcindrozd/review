9a7416f2f1e79024e4bb5532e989ef40106d6e2c
Krzysztof Urban <krzysztof.urban@netguru.pl>
2016-03-13 15:51:08 +0100

    Get rid of custom helpers

--- review_front/tests/unit/helpers/time-relative-test.js
+++
@@ -1,9 +0,0 @@
-import { timeRelative } from '../../../helpers/time-relative';
-import { module, test } from 'qunit';
-module('TimeRelativeHelper');
-
-test('it works', function(assert) {
-  var result;
-  result = timeRelative(42);
-  return assert.ok(result);
-});
