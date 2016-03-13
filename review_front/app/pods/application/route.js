3903c6372cd6b0ea370306a4639328a39f425d2a
Krzysztof Urban <krzysztof.urban@netguru.pl>
2016-03-13 21:19:49 +0100

    Cleanup application pod

--- review_front/app/pods/application/route.js
+++ review_front/app/pods/application/route.js
@@ -1,10 +1,7 @@
 import Ember from 'ember';
-var ApplicationRoute;
 
-ApplicationRoute = Ember.Route.extend({
+export default Ember.Route.extend({
   model() {
     return this.store.find('user', 'me');
   }
 });
-
-export default ApplicationRoute;
