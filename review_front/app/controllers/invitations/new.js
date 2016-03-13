4666a5eab984fa836b7378515bc6dd1445157d81
Krzysztof Urban <krzysztof.urban@netguru.pl>
2016-03-13 14:35:21 +0100

    Fix invitations controller

--- review_front/app/controllers/invitations/new.js
+++ review_front/app/controllers/invitations/new.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-var InvitationsNewController;
 
-InvitationsNewController = Ember.Controller.extend({
+export default Ember.Controller.extend({
   projects: Ember.computed.alias('model.projects'),
   selectedRole: null,
   email: null,
@@ -20,9 +19,7 @@ InvitationsNewController = Ember.Controller.extend({
       return this.get('selectedRole').name === 'contractor';
     }
   }),
-  roleName: Ember.computed('selectedRole', function() {
-    return this.get('selectedRole').name;
-  }),
+  roleName: Ember.computed.alias('selectedRole.name'),
   projectIds: Ember.computed('selectedProjects', 'roleName', function() {
     if (this.get('roleName') !== 'contractor') {
       return null;
@@ -40,19 +37,18 @@ InvitationsNewController = Ember.Controller.extend({
   }),
   actions: {
     create() {
-      var flashMessages;
-      flashMessages = Ember.get(this, 'flashMessages');
-      return this.get('model.invitation').setProperties({
+      const flashMessages = Ember.get(this, 'flashMessages');
+      const invitation = this.get("model.invitation");
+      invitation.setProperties({
         email: this.get('email'),
         role: this.get('roleName'),
         projectIds: this.get('projectIds')
-      }).save().then(function() {
-        return flashMessages.success('Your message was sent!');
-      })["catch"](function() {
-        return flashMessages.danger('Incorrect email address!');
+      });
+      invitation.save().then(function() {
+        flashMessages.success('Your message was sent!');
+      }).catch(function() {
+        flashMessages.danger('Incorrect email address!');
       });
     }
   }
 });
-
-export default InvitationsNewController;
