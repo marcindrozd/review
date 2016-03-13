081bff3cfe3aa915ff354e7d8e9b6aa465125de0
Krzysztof Urban <krzysztof.urban@netguru.pl>
2016-03-13 20:17:46 +0100

    Move users to pods

--- review_front/app/router.js
+++ review_front/app/router.js
@@ -1,23 +1,24 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 const Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
-  this.route('projects');
-  this.route('project', { path: 'projects/:name/commits' });
-  this.route('admin', function() {
-    this.route('invitations', function() {
-      this.route('index', { path: "/" });
-      this.route('new');
+  this.route("projects");
+  this.route("project", { path: "projects/:name/commits" });
+  this.route("admin", function() {
+    this.route("invitations", function() {
+      this.route("index", { path: "/" });
+      this.route("new");
     });
-    this.route('users', { resetNamespace: true }, function() {
-      this.route('edit', { path: '/:nickname' });
+    this.route("users", function() {
+      this.route("index", { path: "/" });
+      this.route("edit", { path: "/:nickname" });
     });
   });
-  this.route('404', { path: '/*path' });
+  this.route("404", { path: "/*path" });
 });
 
 export default Router;
